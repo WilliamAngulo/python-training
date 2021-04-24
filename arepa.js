415cee751f6369a4bcfe9c6b721980d497c87499
= <=>
2021-04-24 15:00:55 -0500

    incorpore saludos a los archivos de pizza y arepa

---
+++ arepa.js
@@ -0,0 +1,9 @@
+const saludo =  "Hola soy una arepa"
+
+const esUnaArepa = (frase) => {
+    let output = false;
+    if(frase === "Hola soy una arepa") {
+        output = true
+    }
+    return output
+}
---
+++ pizza.js
@@ -0,0 +1,1 @@
+const miSaludo = "Hola, soy una pizza"
