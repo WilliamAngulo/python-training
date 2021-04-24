7eebf21d3ed425c058797f070d361109090501f7
= <=>
2021-04-24 15:27:51 -0500

    Crear funcion para validar si es una pizza

--- pizza.js
+++ pizza.js
@@ -1,1 +1,11 @@
 const miSaludo = "Hola, soy una pizza"
+
+const esUnaPizza = (frase) => {
+    let output = false;
+
+    const regexToMatch = /Hola soy una arepa/g
+    if(regexToMatch.test(frase)) {
+        output = true
+    }
+    return output
+}
